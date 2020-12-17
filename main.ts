3c2e9ac23d862cbccb7e7dd8ebfadbc1fe80ce8e
yprazzi <72910828+yprazzi@users.noreply.github.com>
2020-12-17 10:02:14 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,10 @@
+let PI = 3.14159
+let prescision = 2
+let rayon = 4
+let Diametre = prescision * rayon
+let perimetre = Diametre * PI
+let Aire = rayon * rayon * PI
+basic.showString("précision" + "=")
+basic.showString("" + prescision + "/" + "R" + "=" + rayon + "/" + "D" + "=")
+basic.showString("" + Diametre + "/" + "P" + "=" + perimetre)
+basic.showString("/" + "A" + "=" + "=" + Aire)
